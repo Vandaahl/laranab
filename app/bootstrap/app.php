a744e94b4eef059fe54ec3426a8c70934affc17d
Vandaahl <m+github@noo.nu>
2026-08-08 10:42:35 +0200

    Added APP_TRUSTED_PROXIES env variable for configuring trusted IP of a reverse proxy (you can add multiple IP's separated by a comma)

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -6,12 +6,23 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $trustedProxies = array_filter(
+            array_map(
+                'trim',
+                explode(',', getenv('TRUSTED_PROXIES') ?: '')
+            )
+        );
+
+        if (count($trustedProxies)) {
+            $middleware->trustProxies(
+                at: $trustedProxies,
+            );
+        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
